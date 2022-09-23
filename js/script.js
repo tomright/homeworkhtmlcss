ad7764b19f44647c39a16ca56c33d3550ee349b1
TomRight <zxvc@rambler.ru>
2022-09-23 18:01:17 +0500

    Добавил интерактива

--- js/script.js
+++ js/script.js
@@ -1,28 +1,47 @@
 "use strict";
 
 const divContainerJS = document.querySelector(".containerJS");
-const redDiv = document.querySelector('.redMoveObj')
+const redDiv = document.querySelector(".redMoveObj");
+const divRedMoveObj = document.createElement("div");
+// divRedMoveObj.classList.add("redMoveObj");
+// divContainerJS.append(divRedMoveObj);
+// divRedMoveObj.addEventListener("click", playSoundUmaeva);
 
-const count = 26;
-for (let i = 0; i < count; i++) {
-  const moveDiv = document.createElement("div");
-  moveDiv.classList.add("moveObject");
-  divContainerJS.append(moveDiv);
+function createOject() {
+  const count = 26;
+  const randomNum = getRandomFromInterval(0, count);
+
+  divContainerJS.innerHTML = "";
+  for (let i = 0; i < count; i++) {
+    if (i == randomNum) {
+      const moveDiv = document.createElement("div");
+      moveDiv.classList.add("redMoveObj");
+      moveDiv.addEventListener("click", playSoundUmaeva);
+      moveDiv.addEventListener("click", createOject);
+      divContainerJS.append(moveDiv);
+    } else {
+      const moveDiv = document.createElement("div");
+      moveDiv.classList.add("moveObject");
+      moveDiv.addEventListener("click", playSoundNani);
+      moveDiv.addEventListener("click", createOject);
+      divContainerJS.append(moveDiv);
+    }
+  }
 }
-const allMoveDiv = document.querySelectorAll('.moveObject');
+createOject();
 
 function playSoundNani() {
-    let audio = new Audio();
-    audio.src = '/audio/nani.mp3';
-    console.log(audio);
-    audio.autoplay = true;
+  let audio = new Audio();
+  audio.src = "/audio/nani.mp3";
+  console.log(audio);
+  audio.autoplay = true;
 }
 function playSoundUmaeva() {
-let audio = new Audio();
-    audio.src = '/audio/umaev.mp3';
-    console.log(audio);
-    audio.autoplay = true;
+  let audio = new Audio();
+  audio.src = "/audio/umaev.mp3";
+  console.log(audio);
+  audio.autoplay = true;
 }
-allMoveDiv.forEach(val => {
-    val.addEventListener('click', playSoundNani);
-})
+function getRandomFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
